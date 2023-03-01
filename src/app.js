8993e4aaf573b932b1a9ee260d5dedefee954cd3
justinpastine <justin.pastine@gmail.com>
2023-03-01 09:27:49 -0500

    update functions

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,3 +14,91 @@ let board, turn, winner, tie;
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.querySelector('#message');
 const resetBtnEl = document.querySelector('button');
+(_a = document.querySelector('section')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', handleClick);
+resetBtnEl === null || resetBtnEl === void 0 ? void 0 : resetBtnEl.addEventListener('click', init);
+function init() {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0,];
+    turn = 1;
+    console.log('This is the init function');
+    winner = false;
+    tie = false;
+    render();
+}
+init();
+function render() {
+    updateBoard();
+    updateMessage();
+}
+function updateBoard() {
+    board.forEach(function (el, idx) {
+        let square = squareEls[idx];
+        if (board[idx] === 0) {
+            square.textContent = '';
+        }
+        else if (board[idx] === 1) {
+            square.textContent = 'X';
+        }
+        else if (board[idx] === -1) {
+            square.textContent = 'O';
+        }
+    });
+}
+function updateMessage() {
+    let name;
+    if (turn === 1) {
+        name = 'X';
+    }
+    else {
+        name = 'O';
+    }
+    if (winner === false && tie === false) {
+        messageEl.textContent = `It is ${name}'s turn `;
+    }
+    else if (winner === false && tie === true) {
+        messageEl.textContent = "It's a tie!";
+    }
+    else {
+        messageEl.textContent = `Congrats! ${name} won!`;
+    }
+}
+function handleClick(evt) {
+    if (!(evt.target instanceof HTMLElement))
+        return;
+    if (winner === true) {
+        return;
+    }
+    //obtain the index of the square clicked
+    let sqIdx = parseInt(evt.target.id.replace('sq', ''));
+    if (board[sqIdx] !== 0)
+        return;
+    render();
+}
+function placePiece(idx) {
+    board[idx] = turn;
+    console.log(board);
+}
+function checkForTie() {
+    if (!board.some(board => board === 0)) {
+        tie = true;
+    }
+}
+function checkForWinner() {
+    winningCombos.forEach(function (arr) {
+        let winning = 0;
+        arr.forEach(function (el) {
+            winning += board[el];
+        });
+        console.log('check winner', winning);
+        if (Math.abs(winning) === 3) {
+            winner = true;
+        }
+    });
+}
+function switchPlayerTurn() {
+    if (winner === true) {
+        return;
+    }
+    else {
+        turn = turn * -1;
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,105 @@ const winningCombos: number[][] = [
 
 let board: number[], turn: number, winner: boolean, tie: boolean
 
-const squareEls = document.querySelectorAll<HTMLDivElement>('.sqr')
+const squareEls = document.querySelectorAll<HTMLElement>('.sqr')
 
-const messageEl = document.querySelector<HTMLHeadingElement>('#message')
+const messageEl = document.querySelector('#message') as HTMLHeadingElement
 
-const resetBtnEl = document.querySelector<HTMLButtonElement>('button')
+const resetBtnEl = document.querySelector('button') as HTMLButtonElement
 
+document.querySelector<HTMLElement>('section')?.addEventListener('click', handleClick)
+resetBtnEl?.addEventListener('click', init)
+
+function init() {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+  turn = 1
+  console.log('This is the init function')
+  winner = false
+  tie = false
+  render()
+}
+init()
+
+function render() {
+  updateBoard()
+  updateMessage()
+}
+  
+function updateBoard(): void {
+  board.forEach(function(el,idx) {
+    let square = squareEls[idx]
+    if (board[idx] === 0){
+      square.textContent = ''
+    } else if (board[idx] === 1) {
+      square.textContent = 'X'
+    } else if (board[idx] === -1) {
+      square.textContent = 'O'
+    }
+  })
+}
+  
+function updateMessage() {
+  let name
+  if (turn === 1) {
+    name = 'X'
+  }  else {
+    name = 'O'
+  }
+  if (winner === false && tie === false) {
+    messageEl.textContent = `It is ${name}'s turn `
+  } else if (winner === false && tie === true) {
+    messageEl.textContent = "It's a tie!"
+  } else {
+    messageEl.textContent = `Congrats! ${name} won!`
+    
+  }
+}
+
+function handleClick(evt: MouseEvent): void {
+  if (!(evt.target instanceof HTMLElement)) return
+  if (winner === true) {
+    return
+  }
+  
+  //obtain the index of the square clicked
+  
+  let sqIdx: number = parseInt(evt.target.id.replace('sq', ''))
+  
+  if (board[sqIdx] !== 0) return 
+  render()
+  
+}
+
+function placePiece(idx: number): void {
+    board[idx] = turn
+    console.log(board)
+    
+}
+
+  
+function checkForTie(): void {
+  if (!board.some(board => board === 0)) {
+    tie = true
+  }
+}
+
+function checkForWinner(): void {
+  winningCombos.forEach(function(arr){
+    let winning = 0
+    arr.forEach(function(el){
+      winning += board[el]
+    })
+    console.log('check winner', winning)
+    if (Math.abs(winning) === 3) { 
+        winner = true
+    }
+  })
+}
+
+function switchPlayerTurn(): void {
+  if (winner === true){
+    return
+  } else {
+    turn = turn * -1
+  }
+}
